10291f7f51bf817a9943c7dd6a9760ead9cb1dd4
trivm12 <20520821@gm.uit.edu.vn>
2023-05-17 14:37:00 +0700

    First Commit

---
+++ app/src/main/java/com/example/mobile_lab4_20520821/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mobile_lab4_20520821;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mobile_lab4_20520821.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
